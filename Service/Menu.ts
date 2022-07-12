6cb273b0849271b76a44b866c01603a25296f15d
SicdeX <sicdex@gmail.com>
2022-07-12 20:39:18 +0600

    Update Menu.ts

--- Service/Menu.ts
+++ Service/Menu.ts
@@ -3,10 +3,15 @@ import { Menu, UnitData } from "wrapper/Imports"
 export class MENU_UPDATE_HERO {
 
 	public static async UpdateImageSelectorTree(ID: Menu.Dropdown, ImageSelection: Menu.ImageSelector) {
-		const heroes = [...(await UnitData.global_storage).keys()].filter(unitName => unitName !== "npc_dota_hero_base"
-			&& unitName !== "npc_dota_hero_target_dummy"
-			&& unitName.startsWith("npc_dota_hero_"))
+
+		const heroes = [...(await UnitData.global_storage).entries()]
+			.filter(([name, data]) => this.IsValidName(name) && data.AttributePrimary === ID.selected_id).map(([name]) => name)
+
 		ImageSelection.values = heroes
 		await ImageSelection.Update()
+	}
+
+	private static IsValidName(name: string) {
+		return name !== "npc_dota_hero_base" && name !== "npc_dota_hero_target_dummy" && name.startsWith("npc_dota_hero_")
 	}
 }
